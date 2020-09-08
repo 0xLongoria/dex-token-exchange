86aca09478c2954278fe11648fc90fcd5b89a3d6
M Longoria <michaelandrewlongoria@gmail.com>
2020-09-07 22:24:10 -0700

    Refactor Transfer function tests

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,3 +1,5 @@
+import { tokens } from './helpers';
+
 const Token = artifacts.require('./Token');
 
 require('chai')
@@ -8,7 +10,7 @@ contract('Token', ([deployer, receiver]) => {
 	const name = 'DEX Token';
 	const symbol = 'DEX';
 	const decimals = '18';
-	const totalSupply = '1000000000000000000000000';
+	const totalSupply = tokens(1000000).toString();
 	let token;
 
 	beforeEach(async () => {
@@ -33,32 +35,24 @@ contract('Token', ([deployer, receiver]) => {
 
 		it('tracks the total supply', async () => {
 			const result = await token.totalSupply();
-			result.toString().should.equal(totalSupply);
+			result.toString().should.equal(totalSupply.toString());
 		})
 
 		it('assigns the total supply to the deployer', async () => {
 			const result = await token.balanceOf(deployer);
-			result.toString().should.equal(totalSupply);
+			result.toString().should.equal(totalSupply.toString());
 		})
 	})
 
 	describe('sending tokens', () => {
 		it('transfers token balances', async () => {
 			let balanceOf;
-			// Before transfer
-			balanceOf = await token.balanceOf(deployer)
-			console.log("deployer balance before transfer", balanceOf.toString());
-			balanceOf = await token.balanceOf(receiver);
-			console.log("receiver balance before transfer", balanceOf.toString());
-			
 			// Transfer
-			await token.transfer(receiver, '100000000000000000000', { from: deployer})
-
-			// After transfer
+			await token.transfer(receiver, tokens(100), { from: deployer})
 			balanceOf = await token.balanceOf(deployer)
-			console.log("deployer balance after transfer", balanceOf.toString());
+			balanceOf.toString().should.equal(tokens(999900).toString());
 			balanceOf = await token.balanceOf(receiver);
-			console.log("receiver balance after transfer", balanceOf.toString());
+			balanceOf.toString().should.equal(tokens(100).toString());
 
 		})
 	})
