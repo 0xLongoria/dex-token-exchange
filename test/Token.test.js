7aa00bfc52ea90fd725f7b6bb44c8d3f93469005
M Longoria <michaelandrewlongoria@gmail.com>
2020-09-07 20:18:41 -0700

    Create tests on Token contract to check symbol, decimals, total supply

--- test/Token.test.js
+++ test/Token.test.js
@@ -5,12 +5,35 @@ require('chai')
 	.should();
 
 contract('Token', (accounts) => {
+	const name = 'DEX Token';
+	const symbol = 'DEX';
+	const decimals = '18';
+	const totalSupply = '1000000000000000000000000';
+	let token;
+
+	beforeEach(async () => {
+		token = await Token.new();
+	})
 
 	describe('deployment', () => {
 		it('tracks the name', async () => {
-			const token = await Token.new();
 			const result = await token.name();
-			result.should.equal('My Name');
+			result.should.equal(name);
+		})
+
+		it('tracks the symbol', async () => {
+			const result = await token.symbol();
+			result.should.equal(symbol);
+		})
+
+		it('tracks the decimals', async () => {
+			const result = await token.decimals();
+			result.toString().should.equal(decimals);
+		})
+
+		it('tracks the total supply', async () => {
+			const result = await token.totalSupply();
+			result.toString().should.equal(totalSupply);
 		})
 	})
 })
