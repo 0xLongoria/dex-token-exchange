5a5b0d6eed08aef42f6c924a916f1cdc641db61b
M Longoria <michaelandrewlongoria@gmail.com>
2020-09-07 22:46:01 -0700

    Add Transfer event to Token contract and create tests

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,11 +5,15 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract Token {
 	using SafeMath for uint;
 
+	// Variables
 	string public name = "DEX Token";
 	string public symbol = "DEX";
 	uint256 public decimals = 18;
 	uint public totalSupply;
 	mapping(address => uint256) public balanceOf;
+
+	// Events
+	event Transfer(address indexed from, address indexed to, uint256 value);
 
 	constructor() public {
 		totalSupply = 1000000 * (10 ** decimals);
@@ -19,6 +23,7 @@ contract Token {
 	function transfer(address _to, uint256 _value) public returns(bool success) {
 		balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
 		balanceOf[_to] = balanceOf[_to].add(_value);
+		emit Transfer(msg.sender, _to, _value);
 		return true;
 	}
 }
