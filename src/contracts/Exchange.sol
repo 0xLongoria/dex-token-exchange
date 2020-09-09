21c4b9a31e6e11aa45942e8763a0e76241675862
M Longoria <michaelandrewlongoria@gmail.com>
2020-09-09 16:47:59 -0700

    Add functionality to depositToken fxn to send tokens to the exchange

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,18 +1,9 @@
 pragma solidity ^0.5.0;
 
-contract Exchange {
-	// Variables
-	address public feeAccount; // the account that receives exchange fees
-	uint256 public feePercent; // the fee percentage
-
-	constructor (address _feeAccount, uint256 _feePercent) public {
-		feeAccount = _feeAccount;
-		feePercent = _feePercent;
-	}
-}
+import "./Token.sol";
 
 // TODO:
-// [ ] Set the fee account
+// [X] Set the fee account
 // [ ] Deposit Ether
 // [ ] Withdraw Ether
 // [ ] Deposit tokens
@@ -22,3 +13,43 @@ contract Exchange {
 // [ ] Cancel order
 // [ ] Fill order
 // [ ] Charge fees
+
+contract Exchange {
+	// Variables
+	address public feeAccount; // the account that receives exchange fees
+	uint256 public feePercent; // the fee percentage
+
+	constructor (address _feeAccount, uint256 _feePercent) public {
+		feeAccount = _feeAccount;
+		feePercent = _feePercent;
+	}
+
+	function depositToken(address _token, uint _amount) public {
+		Token(_token).transferFrom(msg.sender, address(this), _amount);
+		// Manage deposit - update balance
+		// Send tokens to this contract
+		// Emit event
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
