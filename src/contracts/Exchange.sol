be5a3e30e96a95995d975461275050f2f64a8072
M Longoria <michaelandrewlongoria@gmail.com>
2020-09-09 17:11:53 -0700

    Emit a Deposit event when depositToken fxn is called on Exchange contract

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -17,11 +17,14 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Exchange {
 	using SafeMath for uint;
-	
+
 	// Variables
 	address public feeAccount; // the account that receives exchange fees
 	uint256 public feePercent; // the fee percentage
 	mapping(address => mapping(address => uint256)) public tokens;
+
+	// Events
+	event Deposit(address token, address user, uint256 amount, uint256 balance);
 
 	constructor (address _feeAccount, uint256 _feePercent) public {
 		feeAccount = _feeAccount;
@@ -31,8 +34,7 @@ contract Exchange {
 	function depositToken(address _token, uint _amount) public {
 		require(Token(_token).transferFrom(msg.sender, address(this), _amount));
 		tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
-		// Manage deposit - update balance
-		// Emit event
+		emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
 	}
 
 }
