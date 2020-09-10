0ce8a9451a76568ecfd7ffbe6ac2e1d9bb079cd8
M Longoria <michaelandrewlongoria@gmail.com>
2020-09-09 17:46:50 -0700

    Emit a deposit event when depositing Ether

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -49,6 +49,16 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
 			balance.toString().should.equal(amount.toString());
 		})
 
+		it('emits a Deposit event', async () => {
+				const log = result.logs[0];
+				log.event.should.eq('Deposit');
+				const event = log.args;
+				event.token.should.equal(ETHER_ADDRESS, 'token address is correct');
+				event.user.should.equal(user1, 'user address is correct');
+				event.amount.toString().should.equal(amount.toString(), 'amount is correct');
+				event.balance.toString().should.equal(amount.toString(), 'balance is correct');
+			})
+
 	})
 
 	describe('depositing tokens', () => {
@@ -78,8 +88,8 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
 				const event = log.args;
 				event.token.should.equal(token.address, 'token address is correct');
 				event.user.should.equal(user1, 'user address is correct');
-				event.amount.toString().should.equal(tokens(10).toString(), 'amount is correct');
-				event.balance.toString().should.equal(tokens(10).toString(), 'balance is correct');
+				event.amount.toString().should.equal(amount.toString(), 'amount is correct');
+				event.balance.toString().should.equal(amount.toString(), 'balance is correct');
 			})
 		})
 
