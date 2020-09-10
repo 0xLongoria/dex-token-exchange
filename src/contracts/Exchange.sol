850ff6b4bed52ebfdc87e066a8b5e17aaa64d04a
M Longoria <michaelandrewlongoria@gmail.com>
2020-09-10 13:58:17 -0700

    Add Withdraw event to withdrawEther function. Added rejection for withdraws of insufficient balances

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -26,6 +26,8 @@ contract Exchange {
 
 	// Events
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
+	event Withdraw(address token, address user, uint256 amount, uint256 balance);
+	
 
 	constructor (address _feeAccount, uint256 _feePercent) public {
 		feeAccount = _feeAccount;
@@ -43,7 +45,10 @@ contract Exchange {
 	}
 
 	function withdrawEther(uint _amount) public {
+		require(tokens[ETHER][msg.sender] >= _amount);
 		tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].sub(_amount);
+		msg.sender.transfer(_amount);
+		emit Withdraw(ETHER, msg.sender, _amount, tokens[ETHER][msg.sender]);
 	}
 
 	function depositToken(address _token, uint _amount) public {
