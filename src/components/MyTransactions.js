388187368d75a4a99572a1da0df24e63406cebb5
M Longoria <michaelandrewlongoria@gmail.com>
2020-09-18 14:41:07 -0700

    Display historical trades and open orders on the My Transactions card

--- src/components/MyTransactions.js
+++ src/components/MyTransactions.js
@@ -1,6 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Tabs, Tab } from 'react-bootstrap';
+import Spinner from './Spinner';
+import {
+	myFilledOrdersLoadedSelector,
+	myFilledOrdersSelector,
+	myOpenOrdersLoadedSelector,
+	myOpenOrdersSelector
+} from '../store/selectors';
+
+const showMyFilledOrders = (myFilledOrders) => {
+	return (
+		<tbody>
+			{ myFilledOrders.map((order) => {
+				return (
+					<tr key={order.id}>
+						<td className="text-muted">{order.formattedTimestamp}</td>
+						<td className={`text-${order.orderTypeClass}`}>{order.orderSign}{order.tokenAmount}</td>
+						<td className={`text-${order.orderTypeClass}`}>{order.tokenPrice}</td>
+					</tr>
+				)
+			}) }
+		</tbody>
+	)
+}
+
+const showMyOpenOrders = (myOpenOrders) => {
+	return (
+		<tbody>
+			{ myOpenOrders.map((order) => {
+				return (
+					<tr key={order.id}>
+						<td className={`text-${order.orderTypeClass}`}>{order.tokenAmount}</td>
+						<td className={`text-${order.orderTypeClass}`}>{order.tokenPrice}</td>
+						<td className="text-muted">x</td>
+					</tr>
+				)
+			}) }
+		</tbody>
+	)
+}
 
 class MyTransactions extends Component {
 	render() {
@@ -20,7 +59,7 @@ class MyTransactions extends Component {
 										<th>DEX/EATH</th>
 									</tr>
 								</thead>
-
+								{ this.props.showMyFilledOrders ? showMyFilledOrders(this.props.myFilledOrders) : <Spinner type="table" /> }
 							</table>
 						</Tab>
 						<Tab eventKey="orders" title="Orders">
@@ -32,7 +71,7 @@ class MyTransactions extends Component {
 										<th>Cancel</th>
 									</tr>
 								</thead>
-
+								{ this.props.showMyOpenOrders ? showMyOpenOrders(this.props.myOpenOrders) : <Spinner type="table" /> }
 							</table>
 						</Tab>
 					</Tabs>
@@ -45,7 +84,10 @@ class MyTransactions extends Component {
 function mapStateToProps(state) {
 
 	return {
-		// TODO: Fill me in...
+		myFilledOrders: myFilledOrdersSelector(state),
+		showMyFilledOrders: myFilledOrdersLoadedSelector(state),
+		myOpenOrders: myOpenOrdersSelector(state),
+		showMyOpenOrders: myOpenOrdersLoadedSelector(state)
 	}
 }
 
