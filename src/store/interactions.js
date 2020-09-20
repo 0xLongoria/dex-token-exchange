8f20192b2d004f32ba7d5d65b4c3d71286081d7e
M Longoria <michaelandrewlongoria@gmail.com>
2020-09-20 00:46:53 -0700

    Refactor config settings for network deployment

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -15,7 +15,10 @@ import {
 	exchangeEtherBalanceLoaded,
 	exchangeTokenBalanceLoaded,
 	balancesLoaded,
-	balancesLoading
+	balancesLoading,
+	buyOrderMaking,
+	sellOrderMaking,
+	orderMade
 } from './actions';
 import Token from '../abis/Token.json';
 import Exchange from '../abis/Exchange.json';
@@ -94,6 +97,10 @@ export const subscribeToEvents = async (exchange, dispatch) => {
 
 	exchange.events.Withdraw({}, (error, event) => {
 		dispatch(balancesLoaded());
+	})
+
+	exchange.events.Order({}, (error, event) => {
+		dispatch(orderMade(event.returnValues));
 	})
 }
 
@@ -190,21 +197,34 @@ export const withdrawToken = (dispatch, exchange, web3, token, amount, account) 
 	})
 }
 
+export const makeBuyOrder = (dispatch, exchange, token, web3, order, account) => {
+	const tokenGet = token.options.address;
+	const amountGet = web3.utils.toWei(order.amount, 'ether');
+	const tokenGive = ETHER_ADDRESS;
+	const amountGive = web3.utils.toWei((order.amount * order.price).toString(), 'ether');
 
+	exchange.methods.makeOrder(tokenGet, amountGet, tokenGive, amountGive).send({ from: account })
+	.on('transactionHash', (hash) => {
+		dispatch(buyOrderMaking());
+	})
+	.on('error', (error) => {
+		console.log(error);
+		window.alert(`There was an error!`);
+	})
+}
 
+export const makeSellOrder = (dispatch, exchange, token, web3, order, account) => {
+	const tokenGet = ETHER_ADDRESS;
+	const amountGet = web3.utils.toWei((order.amount * order.price).toString(), 'ether');
+	const tokenGive = token.options.address;
+	const amountGive = web3.utils.toWei(order.amount, 'ether');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	exchange.methods.makeOrder(tokenGet, amountGet, tokenGive, amountGive).send({ from: account })
+	.on('transactionHash', (hash) => {
+		dispatch(sellOrderMaking());
+	})
+	.on('error', (error) => {
+		console.log(error);
+		window.alert(`There was an error!`);
+	})
+}
--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 module.exports = {
   networks: {
@@ -8,6 +10,17 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    kovan: {
+      provider: () => {
+        return new HDWalletProvider({
+          privateKeys: privateKeys.split(','), // Array of account private keys
+          providerOrUrl: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}` // URL to an Ethereum Node
+        })
+      },
+      numberOfAddresses: 2,
+      shareNonce: true,
+      network_id: '42'
     }
   },
   contracts_directory: './src/contracts',
